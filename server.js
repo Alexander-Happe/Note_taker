1498966d7c40a26fbfba3bc6e53fc028204a69c1
Alex Happe <abhdiver1994@gmail.com>
2020-01-06 20:24:20 -0600

    adds the delete functionallitly along with an auto incriment'

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.post('/api/notes', function(req, res){
     fs.readFile('db2.json', function(err, data){
         var json = JSON.parse(data);
         json.push(newNote);
+        console.log(json.length)
+        for(i=0; i < json.length; i++){
+            json[i].id = i + 1
+        }
         fs.writeFile('db2.json', JSON.stringify(json), function(err){
             if(err){
                 console.log(err)
@@ -50,7 +54,28 @@ app.post('/api/notes', function(req, res){
     res.sendFile(path.join(__dirname, 'notes.html'))
 });
 
-
+app.delete('/api/notes/:id', function(req, res){
+    var chosen = req.params.id
+    console.log(chosen)
+    fs.readFile('db2.json', function(err, data){
+        var json = JSON.parse(data);
+        for(i=0; i < json.length; i++){
+            if(chosen == json[i].id){
+                console.log(json[i]);
+                json.splice(i, 1)
+            }
+        }
+        fs.writeFile('db2.json', JSON.stringify(json), function(err){
+            if(err){
+                console.log(err)
+            }
+            else{
+                console.log('Note deleted!')
+            }
+        })
+    })
+    res.sendFile(path.join(__dirname, 'notes.html'))
+});
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
