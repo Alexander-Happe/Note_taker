929c810f87167013774a61731d8b66536ceeca03
Alex Happe <abhdiver1994@gmail.com>
2020-01-06 18:07:10 -0600

    adds all the stuff to get css and all that working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 var PORT = 3000;
@@ -13,6 +14,22 @@ app.get('/', function(req, res){
 
 app.get('/notes', function(req, res){
     res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+app.get("/assets/css/styles.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "assets/css/styles.css"));
+});
+
+app.get('/assets/js/index.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'assets/js/index.js'))
+});
+
+app.get('/db.json', function(req, res){
+    res.sendFile(path.join(__dirname, 'db.json'))
+});
+
+app.get('/api/notes', function(req, res){
+    res.sendFile(path.join(__dirname, 'db.json'))
 })
 
 app.listen(PORT, function(){
